4b135314801e38097923befcb3cf784189722f4a
s2 <root@s2.95xd.com>
2015-08-13 15:28:17 +0800

    asd

--- cmdb/settings.py
+++ cmdb/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cmdb',
-        'USER':'python',
-        'PASSWORD':'!py@xyx',
+        'USER':'root',
+        'PASSWORD':'123456',
         'HOST':'s2.95xd.com',
         'PORT':'3306',
     }
